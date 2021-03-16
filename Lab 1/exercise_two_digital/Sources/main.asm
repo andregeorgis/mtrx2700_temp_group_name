a218563501402ecf67232c156ecd9bde17c39170
andregeorgis <andregeorgis@gmail.com>
2021-03-16 16:04:10 +1100

    working lookup table made - hopefully :)

--- Lab 1/exercise_two_digital/Sources/main.asm
+++ Lab 1/exercise_two_digital/Sources/main.asm
@@ -19,22 +19,25 @@
 
 ROMStart    EQU  $4000  ; absolute address to place my code/constant data
 
-; Define enables for the 7 segments
-NO_SEG      EQU  $FF   ; value to enable no segments
-FIRST_SEG   EQU  $FE   ; value to enable the first segment
-SECOND_SEG  EQU  $FD   ; value to enable the second segment
-THIRD_SEG   EQU  $FB   ; value to enable the third segment
-FOURTH_SEG  EQU  $F7   ; value to enable the fourth segment
-
-; Define the lookup codes for every special character
-;SEG_CODES   FCB $3F, $06, $5B, $4F, $66, $6D, $7D, $07, $7F, $6F, $77, $7C, $39, $5E, $79, $71
-
 ; variable/data section
 
             ORG RAMStart
  ; Insert here your data definition.
 Counter     DS.W 1
 FiboRes     DS.W 1
+
+; Define enables for the 7 segments
+NO_SEG      EQU  $FF   ; value to enable no segments
+FIRST_SEG   EQU  $FE   ; value to enable the first segment
+SECOND_SEG  EQU  $FD   ; value to enable the second segment
+THIRD_SEG   EQU  $FB   ; value to enable the third segment
+FOURTH_SEG  EQU  $F7   ; value to enable the fourth segment
+CURR_SEG    FCB  $0  ; current segment to draw to
+
+; Define the lookup codes for every special character
+SEG_CODES   FCB $3F, $06, $5B, $4F, $66, $6D, $7D, $07, $7F, $6F, $77, $7C, $39, $5E, $79, $71
+CURR_NUM    FCB  $0  ; current number to draw
+CURR_CODE   FCB  $0  ; current seg code for the number to draw
 
 
 ; code section
@@ -49,43 +52,56 @@ _Startup:
             
 ; Configure the direction of ports
 configure:  LDAA    #$FF
-            STAA    DDRB    ; Configure PORTB as output
-            STAA    DDRP    ; Port P as output to enable 7 segments
-
-mainLoop:
+            STAA    DDRB         ; Configure PORTB as output
+            STAA    DDRP         ; Port P as output to enable 7 segments
+            LDX     #SEG_CODES   ; Store the Segment Codes
             
-            LDAB    #$06
-            STAB    PORTB          ; Assign the number 1
-            LDAA    #FIRST_SEG     
-            STAA    PTP            ; Enable the first segment
-            LDAA    #NO_SEG
-            STAA    PTP            ; Disable all segments
-            LDAB    #$5B
-            STAB    PORTB          ; Assign the number 2
-            LDAA    #SECOND_SEG
-            STAA    PTP            ; Enable the second segment
-            LDAA    #NO_SEG
-            STAA    PTP            ; Disable all segments
-            LDAB    #$4F
-            STAB    PORTB          ; Assign the number 3
-            LDAA    #THIRD_SEG     
-            STAA    PTP            ; Enable the third segment
-            LDAA    #NO_SEG
-            STAA    PTP            ; Disable all segments
-            LDAB    #$66
-            STAB    PORTB          ; Assign the number 4
-            LDAA    #FOURTH_SEG
-            STAA    PTP            ; Enable the fourth segment
-            LDAA    #NO_SEG
-            STAA    PTP            ; Disable all segments
-            bra     mainLoop    
+mainLoop:          
+            LDAB    #2
+            STAB    CURR_NUM
+            BSR     Lookup
+            LDAB    #SECOND_SEG
+            STAB    CURR_SEG
+            BSR     DrawOne
+            BRA     mainLoop
             
+               
 
-Lookup:     NOP ; grab number, give back segment code
+delay:      NOP ; naive delay to make the LEDs brighter
+            NOP
+            NOP
+            NOP
+            NOP
+            NOP
+            NOP
+            NOP
+            NOP
+            NOP
+            NOP
+            NOP
+            NOP
+            NOP
+            NOP
+            RTS            
+
+; Given a Current Number, find the segment code needed to draw it
+Lookup:     LDAA    CURR_NUM     ; grab the number
+            LDAB    A, X            ; convert to segment code
+            STAB    CURR_CODE     ; store the segment code
+            RTS
 
 LookupAll:  NOP ; grab a string of numbers, give back a string of segment codes
 
-DrawOne:    NOP ; grab one segment code and specific LED, draw on a specific LED
+; Given a segment code and a specific segment, draw the number on the LED
+DrawOne:    LDAB    CURR_CODE    ; grab one segment code and specific LED, draw on a specific LED
+            LDAA    CURR_SEG     ; grab the current segment
+            STAB    PORTB        ; assign the number
+            STAA    PTP          ; enable the current LED
+            BSR     delay
+            LDAA    #NO_SEG       
+            STAA    PTP          ; disable all LEDs
+            RTS
+            
 
 DrawFour:   NOP ; grab four segment codes, draw on each LED
 
@@ -94,6 +110,7 @@ CheckButton:NOP ; see if the button active or deactive
 NumberLoop: NOP ; if button is pressed, this should keep showing all numbers on LED
 
 Scroll:     NOP ; grab a string, call LookupAll, keep calling DrawFour until end of string
+
 
 
 ;**************************************************************
